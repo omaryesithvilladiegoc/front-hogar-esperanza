a7276011263b3782e83ce55212d1f94a08bf69b9
omaryesithvilladiego <omaryesithvilladiegocarrascal@outlook.com>
2025-01-07 15:38:18 -0500

    fixed gallery

--- src/components/LadingPage/gallery/index.tsx
+++ src/components/LadingPage/gallery/index.tsx
@@ -5,7 +5,7 @@ import { Box, Button, Modal, IconButton, Typography, useMediaQuery } from "@mui/
 import AddPhotoAlternateIcon from '@mui/icons-material/AddPhotoAlternate';
 import CloseIcon from '@mui/icons-material/Close';
 import '@/styles/gallery.css';
-import Grid from '@mui/material/Grid2';
+import Grid from '@mui/material/Grid';
 import ViewAgendaIcon from '@mui/icons-material/ViewAgenda';
 import ViewColumnIcon from '@mui/icons-material/ViewColumn';
 import { fontRoboto } from '@/config/fonts';
@@ -27,8 +27,7 @@ const additionalImages = [
   'https://res.cloudinary.com/de5tm90td/image/upload/q_auto,f_auto/v1736117377/gallery/WhatsApp_Image_2025-01-05_at_5.46.38_PM_8_ugf4bl.jpg'
 ];
 
-
-const Gallery:React.FC = () => {
+const Gallery: React.FC = () => {
   const matches = useMediaQuery("(max-width:800px)");
   const [open, setOpen] = useState(false);
   const [gridSize, setGridSize] = useState(4);
@@ -37,92 +36,88 @@ const Gallery:React.FC = () => {
   const handleClose = () => {
     setOpen(false);
     setGridSize(6)
-  } 
+  };
 
   const toggleGridSize = () => {
     setGridSize(gridSize === 4 ? 6 : gridSize === 6 ? 4 : 6);
   };
 
   return (
-    <Box id="gallery"  className="gallery" sx={{  minHeight: '100vh', color: 'white', p: 5, display: 'flex', flexDirection: 'column' }}>
-      <Grid container spacing={2} sx={{ flexGrow: 1, width:'100%',  height:'20rem', justifyContent:'center'}}>
-        <Grid  size={{xs:12,sm:6,md:6}}  sx={{ display: 'flex', flexDirection: 'column' }}>
-          <div
-            {...imageProps}
-            className='animation-image'
-            style={{
-              background: `url('https://res.cloudinary.com/de5tm90td/image/upload/f_auto,q_auto,w_800/v1/gallery/jp60f4h7sycjixspdhrz')`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              width: '100%',  // Ajusta según el tamaño deseado
-              height: '100%', // Ajusta según el tamaño deseado
-               borderRadius:'2rem'
-            }}
-          ></div>
-        </Grid>
-        <Grid size={{xs:12,sm:6,md:6}} sx={{ display: 'flex', flexDirection: 'column' }}>
+    <Box id="gallery" className="gallery" sx={{ minHeight: '100vh', color: 'white', p: 5, display: 'flex', flexDirection: 'column' }}>
+     <Grid container spacing={2} sx={{ flexGrow: 1, width: '100%', height: '20rem', justifyContent: 'center' }}>
+  <Grid item xs={12} sm={6} md={6} sx={{ display: 'flex', flexDirection: 'column' }}>
+    <div
+      {...imageProps}
+      className='animation-image'
+      style={{
+        background: `url('https://res.cloudinary.com/de5tm90td/image/upload/q_auto,f_auto/v1736117377/gallery/WhatsApp_Image_2025-01-05_at_5.46.38_PM_7_yvtxd5.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '100%',
+        borderRadius: '2rem'
+      }}
+    ></div>
+  </Grid>
+  <Grid item xs={12} sm={6} md={6} sx={{ display: 'flex', flexDirection: 'column' }}>
+    <div
+      {...imageProps}
+      className='animation-image'
+      style={{
+        background: `url('https://res.cloudinary.com/de5tm90td/image/upload/q_auto,f_auto/v1736117380/gallery/WhatsApp_Image_2025-01-05_at_5.46.38_PM_2_khf4sf.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '100%',
+        borderRadius: '2rem'
+      }}
+    ></div>
+  </Grid>
+  <Grid item xs={12} sm={6} md={8} sx={{ display: 'flex', flexDirection: 'column' }}>
+    <div
+      {...imageProps}
+      className='animation-image'
+      style={{
+        background: `url('https://res.cloudinary.com/de5tm90td/image/upload/q_auto,f_auto/v1736117381/gallery/WhatsApp_Image_2025-01-05_at_5.46.38_PM_1_uz1jd1.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '100%',
+        borderRadius: '2rem'
+      }}
+    ></div>
+  </Grid>
+  <Grid item xs={12} sm={6} md={4} sx={{ display: 'flex', flexDirection: 'column' }}>
+    <div
+      {...imageProps}
+      className='animation-image'
+      style={{
+        background: `url('https://res.cloudinary.com/de5tm90td/image/upload/q_auto,f_auto/v1736117377/gallery/WhatsApp_Image_2025-01-05_at_5.46.38_PM_8_ugf4bl.jpg')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat',
+        width: '100%',
+        height: '100%',
+        borderRadius: '2rem'
+      }}
+    ></div>
+  </Grid>
+</Grid>
 
-        <div
-            {...imageProps}
-            className='animation-image'
-            style={{
-              background: `url('https://res.cloudinary.com/de5tm90td/image/upload/f_auto,q_auto,w_800/v1/gallery/x7vqo4wxuylspkjw4imh')`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              width: '100%',  // Ajusta según el tamaño deseado
-              height: '100%', // Ajusta según el tamaño deseado
-               borderRadius:'2rem'
-            }}
-          ></div>
-        </Grid>
-        <Grid size={{xs:12,sm:6,md:8}} sx={{ display: 'flex', flexDirection: 'column' }}>
-      
-           <div
-            {...imageProps}
-            className='animation-image'
-            style={{
-              background: `url('https://res.cloudinary.com/de5tm90td/image/upload/f_auto,q_auto,w_800/v1/gallery/ws8th2tzkmkmsaxiskak')`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              width: '100%',  // Ajusta según el tamaño deseado
-              height: '100%', // Ajusta según el tamaño deseado
-               borderRadius:'2rem'
-            }}
-          ></div>
-        </Grid>
-        <Grid size={{xs:12,sm:6,md:4}} sx={{ display: 'flex', flexDirection: 'column' }}>
-       
-
-<div
-            {...imageProps}
-            className='animation-image'
-            style={{
-              background: `url('https://res.cloudinary.com/de5tm90td/image/upload/f_auto,q_auto,w_800/v1/gallery/vulrlgtxbfuupiquqrjg')`,
-              backgroundSize: 'cover',
-              backgroundPosition: 'center',
-              backgroundRepeat: 'no-repeat',
-              width: '100%',  // Ajusta según el tamaño deseado
-              height: '100%', // Ajusta según el tamaño deseado
-              borderRadius:'2rem'
-            }}
-          ></div>
-        </Grid>
-      </Grid>
       <button className={fontRoboto.className} style={{
-      width:matches ? '80%' : '20%',
-      borderRadius:'10px',
-      margin:'0 auto',
-      color:'white',
-      backgroundColor:'#164d34',
-      border:'.5px solid white',
-      marginTop:'1rem',
-      padding:'.5rem'
-      
-    }} onClick={handleOpen} > Ver más fotos <AddPhotoAlternateIcon sx={{ fontSize: 20 }} /></button>
-  
+        width: matches ? '80%' : '20%',
+        borderRadius: '10px',
+        margin: '0 auto',
+        color: 'white',
+        backgroundColor: '#164d34',
+        border: '.5px solid white',
+        marginTop: '1rem',
+        padding: '.5rem'
+      }} onClick={handleOpen} > Ver más fotos <AddPhotoAlternateIcon sx={{ fontSize: 20 }} /></button>
+
       <Modal
         open={open}
         onClose={handleClose}
@@ -158,28 +153,26 @@ const Gallery:React.FC = () => {
             onClick={toggleGridSize}
             sx={{ mb: 2 }}
           >
-         { gridSize === 6 ? < ViewColumnIcon  style={{  position:'fixed' }}/> : < ViewAgendaIcon  style={{  position:'fixed' }}/>}
-
+            {gridSize === 6 ? < ViewColumnIcon style={{ position: 'fixed' }} /> : < ViewAgendaIcon style={{ position: 'fixed' }} />}
           </Button>
-          <Grid style={{ height: '80dvh', width:'60dvw' }} container spacing={1}>
-  {additionalImages.map((src, index) => (
-    <Grid size={{xs:12,sm:4, md:gridSize}}  key={index}>
-      <div
-      className='animation-image'
-        style={{
-          
-          background: `url('${src}')`,
-          backgroundSize: 'cover',
-          backgroundPosition: 'center',
-          backgroundRepeat: 'no-repeat',
-          borderRadius: '2rem',
-          width: '100%',
-          height: '50vh', 
-        }}
-      ></div>
-    </Grid>
-  ))}
-</Grid>
+          <Grid style={{ height: '80dvh', width: '60dvw' }} container spacing={1}>
+            {additionalImages.map((src, index) => (
+              <Grid item xs={12} sm={4} md={gridSize} key={index}>
+                <div
+                  className='animation-image'
+                  style={{
+                    background: `url('${src}')`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                    backgroundRepeat: 'no-repeat',
+                    borderRadius: '2rem',
+                    width: '100%',
+                    height: '50vh',
+                  }}
+                ></div>
+              </Grid>
+            ))}
+          </Grid>
         </Box>
       </Modal>
     </Box>
