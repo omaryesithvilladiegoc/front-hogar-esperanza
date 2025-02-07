0ce52cc58c5f34022b974694acf1f16f4c69ffcd
omaryesithvilladiego <omaryesithvilladiegocarrascal@outlook.com>
2025-02-07 08:29:24 -0500

    update site cnfig

--- config/site.ts
+++ config/site.ts
@@ -1,7 +1,7 @@
 export type SiteConfig = typeof siteConfig;
 
 export const siteConfig = {
-  name: "Hogar Esperanza",
+  name: "Hogar Esperanza - Cuidado de Adultos Mayores",
   navItems: [
     {
       label: "Home",
